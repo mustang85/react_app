c42f9c2461b1242b0cc2fb0043417a11d4012c93
mustang85 <sergeant-kir@mail.ru>
2017-02-27 17:05:30 +0300

    delete data on the server with fetch

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import {
   addTodo, generateId, findById,
   toggleTodo, updateTodo, removeTodo,
   filterTodos } from './lib/todoHelpers';
-import {pipe, partial} from './lib/utils';
-import {loadTodos, createTodo, saveTodo} from './lib/todoService';
+import { pipe, partial } from './lib/utils';
+import { loadTodos, createTodo, saveTodo, destroyTodo } from './lib/todoService';
 
 
 class App extends Component {
@@ -23,7 +23,9 @@ class App extends Component {
   handleRemove = (id, evt) => {
     evt.preventDefault();
     const updatedTodos = removeTodo(this.state.todos, id);
-    this.setState({todos: updatedTodos})
+    this.setState({todos: updatedTodos});
+    destroyTodo(id)
+      .then(() => this.showTempMessage('Todo Removed'));
   }
 
   handleInputChange = (e) => {
